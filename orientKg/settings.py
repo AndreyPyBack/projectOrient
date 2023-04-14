027d62a71c23d3fbab595a74ca10abc083724a2a
AndreyPyBack <duha120103@gmail.com>
2023-04-14 17:07:42 +0300

    ff

--- orientKg/settings.py
+++ orientKg/settings.py
@@ -119,11 +119,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# settings.py
+
+# Указываем URL, по которому будут доступны статические файлы
 STATIC_URL = '/static/'
+
+# Указываем путь к директории, в которую будут собраны статические файлы
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Указываем дополнительные директории, в которых Django будет искать статические файлы
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'), # Пример пути к дополнительной директории со статическими файлами
 ]
+
 
 
 
